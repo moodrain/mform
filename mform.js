9d4d5f6ceafc5c0ec2e9bef80c9fb27759210c63
Moodrain <moerain@qq.com>
2020-06-17 11:05:23 +0800

    Update mform.js

--- mform.js
+++ mform.js
@@ -2,7 +2,7 @@ function $mform(mform, container, callback) {
     let inputWrapper = $vdf(mform.inputWrapper, defaultInputWrapper)
     let formWrapper = $vdf(mform.formWrapper, defaultFormWrapper)
     let btnWrapper = $vdf(mform.btnWrapper, defaultBtnWrapper)
-    if(!mform.notifier)
+    if (!mform.notifier)
         importMuyuCss()
     mform.notifier = $vdf(mform.notifier, {
         success: msg => { $notify(msg) },
@@ -11,14 +11,14 @@ function $mform(mform, container, callback) {
     let inputs = []
     let btns = []
     mform.mform = mform
-    if(mform.inputWrapper === undefined || mform.formWrapper === undefined)
+    if (mform.inputWrapper === undefined || mform.formWrapper === undefined)
         importPureCss()
     mform.fields = initFields(mform.fields)
     mform.btns = $vlt(mform.btns, b => initBtns(b))
     mform.verify = mform.verify ? initVerify(mform) : mform.verify
     mform.fields.forEach(field => { inputs.push(inputWrapper(createInput(field))) })
     mform.fields = inputs
-    if(mform.btns) {
+    if (mform.btns) {
         mform.btns.forEach(btn => { btns.push(btnWrapper(createBtn(btn, mform))) })
         mform.btns = btns
     }
@@ -28,21 +28,21 @@ function $mform(mform, container, callback) {
         return $fd(keys, $vdf(mform.object.prefix, undefined))
     }
     mform = mform.mform = formWrapper(createForm(mform))
-    if(container)
+    if (container)
         $add($e(container), mform.tag)
-    if(mform.object.api && mform.object.load !== false) {
-        if(!mform.object.values)
+    if (mform.object.api && mform.object.load !== false) {
+        if (!mform.object.values)
             mform.object.values = {}
         $get(mform.object.api, rs => {
-            for([key, val] of Object.entries(rs.data))
+            for ([key, val] of Object.entries(rs.data))
                 mform.object.values[key] = val
             setValues(mform.object)
         })
     } else {
-        if(mform.object.values)
+        if (mform.object.values)
             setValues(mform.object)
     }
-    if(callback)
+    if (callback)
         callback(mform)
     return mform
 
@@ -50,38 +50,39 @@ function $mform(mform, container, callback) {
         form.fields.forEach(field => { $v(field.object.id, '') })
         setValues(form)
     }
+
     function setValues(form) {
-        for([key, val] of Object.entries(form.values))
-        {
-            if(form.format)
-                for([fKey, formatter] of Object.entries(form.format))
-                    if(fKey == key)
+        for ([key, val] of Object.entries(form.values)) {
+            if (form.format)
+                for ([fKey, formatter] of Object.entries(form.format))
+                    if (fKey == key)
                         val = formatter(val)
             $v(form.prefix ? form.prefix + '-' + key : key, val)
         }
     }
+
     function initFields(rawFields) {
         let fields = []
         rawFields.forEach(rawField => {
             let fd = rawField.split(':')
-            let key, id, name, tag, type, options, attributes, placeholder,checkboxVal
+            let key, id, name, tag, type, options, attributes, placeholder, checkboxVal
             key = fd[0]
             id = (mform.prefix ? mform.prefix + '-' + fd[1] : fd[1]).split('>')[0]
             name = fd[1].split('>')[0]
-            tag = $vex(fd[2], 'input', rs => rs !== undefined, elem =>  elem.split('|')[0].split('>')[0])
-            switch(tag) {
+            tag = $vex(fd[2], 'input', rs => rs !== undefined, elem => elem.split('|')[0].split('>')[0])
+            switch (tag) {
                 case 'text':
                 case 'number':
                 case 'date':
                 case 'datetime':
                 case 'file':
                 case 'checkbox':
-                case 'password': 
+                case 'password':
                     type = tag
                     tag = 'input'
             }
             type = $vdf(type, 'text')
-            if(tag == 'select') {
+            if (tag == 'select') {
                 optionKVs = rawField.split('|')[1].split(',')
                 options = {}
                 optionKVs.forEach(optionKV => {
@@ -89,20 +90,21 @@ function $mform(mform, container, callback) {
                     options[optionKV[0]] = optionKV[1]
                 })
             }
-            if(type == 'checkbox') {
+            if (type == 'checkbox') {
                 name = name + '[]'
                 checkboxVal = $vdf(rawField.split('|')[1], id)
             }
             attributes = $vlt(rawField.split('>')[1], e => e.split('|')[0], e => e.split(','))
-            if(attributes)
-                for(let i = 0;i < attributes.length;i++)
-                    if(attributes[i] == 'readonly')
+            if (attributes)
+                for (let i = 0; i < attributes.length; i++)
+                    if (attributes[i] == 'readonly')
                         attributes[i] = 'readOnly'
             placeholder = $vdf(fd[3], undefined)
-            fields.push({key, id, name, tag, type, options, attributes, placeholder,checkboxVal})
+            fields.push({ key, id, name, tag, type, options, attributes, placeholder, checkboxVal })
         })
         return fields
     }
+
     function initBtns(rawBtns) {
         let btns = []
         rawBtns.forEach(rawBtn => {
@@ -117,14 +119,15 @@ function $mform(mform, container, callback) {
         })
         return btns
     }
+
     function initVerify(form) {
         let rules = {}
-        for([key, val] of Object.entries(form.verify)) {
+        for ([key, val] of Object.entries(form.verify)) {
             val = typeof val == 'function' ? [val] : val.split(',')
-            if(key == 'default') {
+            if (key == 'default') {
                 form.fields.forEach(field => {
-                    switch(field.name) {
-                        case 'phone': 
+                    switch (field.name) {
+                        case 'phone':
                             rules.phone = $vdf(rules.phone, [])
                             rules.phone.push('phone')
                             break
@@ -146,8 +149,7 @@ function $mform(mform, container, callback) {
                             break
                     }
                 })
-            }
-            else if(key == 'all') {
+            } else if (key == 'all') {
                 form.fields.forEach(field => {
                     key = field.name
                     rules[key] = $vdf(rules[key], [])
@@ -160,6 +162,7 @@ function $mform(mform, container, callback) {
         }
         return rules
     }
+
     function defaultFormWrapper(form) {
         let tag = form.tag
         let fieldset = $tag('fieldset')
@@ -169,7 +172,7 @@ function $mform(mform, container, callback) {
             $add(fieldset, field.tag)
         })
         control.className = 'pure-controls'
-        if(form.object.btns)
+        if (form.object.btns)
             form.object.btns.forEach(btn => {
                 $add(control, btn.tag)
             })
@@ -177,27 +180,30 @@ function $mform(mform, container, callback) {
         $add(tag, fieldset)
         return form
     }
+
     function defaultInputWrapper(input) {
         input.div.className = 'pure-control-group'
         return input
     }
+
     function defaultBtnWrapper(btn) {
-        if(!btn.object.className)
+        if (!btn.object.className)
             btn.tag.className = btn.object.className = 'pure-button pure-button-primary'
         return btn
     }
+
     function createForm(form) {
         let tag = $tag('form')
         tag.action = form.action = $vdf(form.action, '')
         tag.method = form.method = $vdf(form.method, 'post')
-        if(form.id)
+        if (form.id)
             tag.id = form.id
-        if(form.name)
+        if (form.name)
             tag.name = form.name
-        if(form.className)
+        if (form.className)
             tag.className = form.className
         form.enctype = $vdf(form.enctype, 'form')
-        switch(form.enctype) {
+        switch (form.enctype) {
             case 'form':
                 form.enctype = tag.enctype = 'application/x-www-form-urlencoded'
                 break
@@ -207,8 +213,9 @@ function $mform(mform, container, callback) {
             case 'data':
                 form.enctype = tag.enctype = 'multipart/form-data'
         }
-        return {tag, object: form}
+        return { tag, object: form }
     }
+
     function createInput(input) {
         let tag = $tag(input.tag)
         let label = $tag('label', input.key)
@@ -217,40 +224,55 @@ function $mform(mform, container, callback) {
         tag.name = input.name
         label.for = input.id
         input.attributes = $vdf(input.attributes, [])
-        switch(input.name) {
-            case 'phone': input.attributes.push('phone')
-            case 'email': input.attributes.push('email')
-            case 'name': input.attributes.push('name')
+        switch (input.name) {
+            case 'phone':
+                input.attributes.push('phone')
+            case 'email':
+                input.attributes.push('email')
+            case 'name':
+                input.attributes.push('name')
             case 'rePassword':
-            case 'password': input.attributes.push('password')
+            case 'password':
+                input.attributes.push('password')
         }
-        if(input.placeholder)
+        if (input.placeholder)
             tag.placeholder = input.placeholder
-        if(input.type)
+        if (input.type)
             tag.type = input.type
-        if(input.options) {
-            for([key, val] of Object.entries(input.options)) {
+        if (input.options) {
+            for ([key, val] of Object.entries(input.options)) {
                 let optionTag = $tag('option')
                 optionTag.innerHTML = key
                 optionTag.value = val
                 $add(tag, optionTag)
             }
         }
-        if(input.checkboxVal)
+        if (input.checkboxVal)
             tag.value = input.checkboxVal
-        if(input.attributes) {
+        if (input.attributes) {
             input.attributes.forEach(attribute => {
-                switch(attribute) {
-                    case 'autooff': tag.autocomplete = 'off';break
-                    case 'phone': tag.autocomplete = 'tel-national';break
-                    case 'email': tag.autocomplete = 'email';break
-                    case 'name': tag.autocomplete = 'name';break
-                    case 'password': tag.autocomplete = 'new-password';break
-                    default: tag[attribute] = true
+                switch (attribute) {
+                    case 'autooff':
+                        tag.autocomplete = 'off';
+                        break
+                    case 'phone':
+                        tag.autocomplete = 'tel-national';
+                        break
+                    case 'email':
+                        tag.autocomplete = 'email';
+                        break
+                    case 'name':
+                        tag.autocomplete = 'name';
+                        break
+                    case 'password':
+                        tag.autocomplete = 'new-password';
+                        break
+                    default:
+                        tag[attribute] = true
                 }
             })
         }
-        if(input.placeholder) {
+        if (input.placeholder) {
             tag.placeholder = input.placeholder
         }
         $add(div, label, tag)
@@ -262,31 +284,40 @@ function $mform(mform, container, callback) {
             object: input,
         }
     }
+
     function createBtn(btn, form) {
         let tag = $tag('button')
         let verify = (rule, val) => {
-            if(typeof rule == 'function')
+            if (typeof rule == 'function')
                 return rule(val).result
-            switch(rule) {
-                case 'phone': return /^(((13[0-9]{1})|15[0-9]{1}|18[0-9]{1}|)+\d{8})$/.test(val)
-                case 'email': return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val)
-                case 'repassword': return form.mform.tag.password.value === form.mform.tag.rePassword.value
-                case 'notnull': return val !== '' && val !== null
+            switch (rule) {
+                case 'phone':
+                    return /^(((13[0-9]{1})|15[0-9]{1}|18[0-9]{1}|)+\d{8})$/.test(val)
+                case 'email':
+                    return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val)
+                case 'repassword':
+                    return form.mform.tag.password.value === form.mform.tag.rePassword.value
+                case 'notnull':
+                    return val !== '' && val !== null
             }
         }
         let getMsg = (rule, key, val) => {
             form.mform.object.fields.forEach(field => {
-                if(field.object.name == key)
+                if (field.object.name == key)
                     key = field.object.key
             })
-            if(typeof rule == 'function')
+            if (typeof rule == 'function')
                 return rule(val).message
-            switch(rule) {
-                case 'notnull': return key + '不能为空'
+            switch (rule) {
+                case 'notnull':
+                    return key + '不能为空'
                 case 'phone':
-                case 'email': return key + '不符合格式'
-                case 'repassword': return '两次输入密码不相同'
-                default: return '验证失败，未知错误'
+                case 'email':
+                    return key + '不符合格式'
+                case 'repassword':
+                    return '两次输入密码不相同'
+                default:
+                    return '验证失败，未知错误'
             }
         }
         tag.type = 'button'
@@ -294,61 +325,67 @@ function $mform(mform, container, callback) {
         tag.id = btn.id
         tag.name = btn.name
         tag.className = btn.className
-        switch(btn.type) {
-            case 'reset': $click(tag, () => { reset(form) });break
-            case 'submit': $click(tag, () => {
-                let tag = form.mform.tag
-                let obj = form.mform.object
-                let success = obj.notifier.success
-                let error = obj.notifier.error
-                let rs = true
-                if(obj.verify) {
-                    let rules = form.mform.object.verify
-                    let fd = obj.formData()
-                    for([key, rule] of Object.entries(rules)) {
-                        for([field, val] of Object.entries(fd)) {
-                            if(field == key) {
-                                for(let i = 0;i < rule.length;i++)
-                                    if(!verify(rule[i], val)) {
-                                        rs = false
-                                        form.mform.object.notifier.error(getMsg(rule[i], key, val))
-                                        break
-                                    }
+        switch (btn.type) {
+            case 'reset':
+                $click(tag, () => { reset(form) });
+                break
+            case 'submit':
+                $click(tag, () => {
+                    let tag = form.mform.tag
+                    let obj = form.mform.object
+                    let success = obj.notifier.success
+                    let error = obj.notifier.error
+                    let rs = true
+                    if (obj.verify) {
+                        let rules = form.mform.object.verify
+                        let fd = obj.formData()
+                        for ([key, rule] of Object.entries(rules)) {
+                            for ([field, val] of Object.entries(fd)) {
+                                if (field == key) {
+                                    for (let i = 0; i < rule.length; i++)
+                                        if (!verify(rule[i], val)) {
+                                            rs = false
+                                            form.mform.object.notifier.error(getMsg(rule[i], key, val))
+                                            break
+                                        }
+                                }
                             }
                         }
                     }
-                }
-                if(rs) {
-                    if(obj.api)
-                        $post(obj.api, obj.formData(), rs => { success($vdf(rs.msg , '提交成功')) }, rs => { error($vdf(rs.msg, '提交失败')) }, obj.enctype == 'application/json' ? 'json' : 'form')
-                    else
-                        tag.submit()
-                }
-            })
+                    if (rs) {
+                        if (obj.api)
+                            $post(obj.api, obj.formData(), rs => { success($vdf(rs.msg, '提交成功')) }, rs => { error($vdf(rs.msg, '提交失败')) }, obj.enctype == 'application/json' ? 'json' : 'form')
+                        else
+                            tag.submit()
+                    }
+                })
         }
         return {
-            tag, object: btn
+            tag,
+            object: btn
         }
     }
+
     function importPureCss() {
         let links = document.querySelectorAll('link')
-        for(let i = 0;i < links.length;i++)
-            if(links[i].href.indexOf('pure.css') >= 0 || links[i].href.indexOf('pure.min.css') >= 0)
+        for (let i = 0; i < links.length; i++)
+            if (links[i].href.indexOf('pure.css') >= 0 || links[i].href.indexOf('pure.min.css') >= 0)
                 return
         $l('未发现pure的link标签，将通过ajax加载 | the link tag of pure not found, loading from ajax')
-        $get('https://unpkg.com/purecss@1.0.0/build/pure-min.css', {}, rs => {
+        $get('https://s1.moodrain.cn/lib/pure/index.css', {}, rs => {
             let css = $tag("style", rs)
             css.type = "text/css"
             $add(document.body, css)
         }, () => {}, false)
     }
+
     function importMuyuCss() {
         let links = document.querySelectorAll('link')
-        for(let i = 0;i < links.length;i++)
-            if(links[i].href.indexOf('muyu.css'))
+        for (let i = 0; i < links.length; i++)
+            if (links[i].href.indexOf('muyu.css'))
                 return
         $l('未发现muyu的link标签，将通过ajax加载 | the link tag of muyu not found, loading from ajax')
-        $get('https://s1.moodrain.cn/lib/muyu.css', {}, rs => {
+        $get('https://s1.moodrain.cn/lib/muyu/index.css', {}, rs => {
             let css = $tag("style", rs)
             css.type = "text/css"
             $add(document.body, css)
